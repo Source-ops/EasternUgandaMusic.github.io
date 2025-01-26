09415a900300448101aa1d720e2691a733b1b918
Source-ops <profelixsnow@gmail.com>
2025-01-26 19:59:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// page loader
+setTimeout(() =>{
+    const loaderContainer = document.querySelector('.loader-container');
+    const websiteContent = document.querySelector('.website-content');
+    loaderContainer.style.display ='none';
+}, 9000);
+
+function shareArtist(){
+    alert("This feature is under development! You will be NOTIFIED with time... Thanks");
+}
